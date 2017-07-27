9fc1e21419a45dcbfc39fe3db36687e918aa7514
Merrick Sapsford <merrick@sapsford.tech>
2017-07-27 11:57:27 +0100

    Update example playground

--- Listenable Example.playground/Contents.swift
+++ Listenable Example.playground/Contents.swift
@@ -1,5 +1,49 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
+import XCPlayground
+import Listenable
 
-var str = "Hello, playground"
+/*:
+ # Listenable
+ **A Swift object that provides an observable platform for multiple listeners.**
+ 
+ This playground shows some of the useful observable behaviour that **Listenable** can be used for.
+ 
+ ### Basics
+ 
+ Create an object that you want to make observable, and make it inherit from Listenable, parameterized with the protocol that observers must conform to.
+ */
+protocol ObjectObservable {
+    
+    func doStuff()
+}
+
+class Object: Listenable<ObjectObservable> {
+    
+}
+/*:
+ With an instance of your observable object, you can then attach listeners that will receive updates when prompted.
+ */
+class Listener: ObjectObservable {
+    
+    func doStuff() {
+        print("Update Received")
+    }
+}
+
+let observableObject = Object()
+let listener = Listener()
+
+observableObject.add(listener: listener)
+
+/*:
+ Then you can simply prompt the listeners with updates whenever you require from your observable object.
+ */
+
+extension Object {
+    
+    func action() {
+        updateListeners { (listener, index) in
+            listener.doStuff()
+        }
+    }
+}
